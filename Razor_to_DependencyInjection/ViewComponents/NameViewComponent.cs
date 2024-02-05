d98e32d736871ff044305f350396b57c5de9b2d0
MBoota <m43577535@gmail.com>
2024-02-05 11:08:44 +0500

    Rendring ViewCompnent Name in HOme View

--- Razor_to_DependencyInjection/ViewComponents/NameViewComponent.cs
+++ Razor_to_DependencyInjection/ViewComponents/NameViewComponent.cs
@@ -4,11 +4,13 @@ namespace Razor_to_DependencyInjection.ViewComponents
 {
     public class NameViewComponent:ViewComponent
     {
-        Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             return View();
             //this component search or invoke partialView at this location
             //shared/Components/NameASMatch/Default.cshtml
+            //________ Note _____
+            //if we gave different name to view then we have to gave complete 
         }
     }
 }
