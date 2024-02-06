5b8728311f9fc3faca62711a23c85c5de132a992
MBoota <m43577535@gmail.com>
2024-02-06 10:57:21 +0500

    Adding ViewComponnet

---
+++ Razor_to_DependencyInjection/ViewComponents/NameViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Razor_to_DependencyInjection.ViewComponents
+{
+    public class NameViewComponent:ViewComponent
+    {
+        Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
