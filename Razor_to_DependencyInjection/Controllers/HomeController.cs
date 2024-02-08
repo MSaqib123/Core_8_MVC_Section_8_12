a20c15a04688c5a1363e235add30f3dc58632054
MBoota <m43577535@gmail.com>
2024-02-08 19:58:39 +0500

    Adding Configuration

--- Razor_to_DependencyInjection/Controllers/HomeController.cs
+++ Razor_to_DependencyInjection/Controllers/HomeController.cs
@@ -103,7 +103,7 @@ namespace Razor_to_DependencyInjection.Controllers
         //=========================================================================
         //========================== Section = Dependency Inject ==========================
         //=========================================================================
-        #region View_Compoentn
+        #region DI
         //======= Class 1 =======
         private readonly ICitiesService _cities;
         public HomeController(ICitiesService cities)
@@ -119,5 +119,28 @@ namespace Razor_to_DependencyInjection.Controllers
         }
         #endregion
 
+
+        //=========================================================================
+        //========================== Section = Envirment ==========================
+        //=========================================================================
+        #region Envirment
+        //Enirment are used in Working ,with  Program (startup) file
+        //===== 1. Developement Enirment =========
+        //This environment is where software developers write, test, and debug code. It is often a local setup on developers' machines or a dedicated development server. In this environment, developers have the flexibility to experiment, make changes, and troubleshoot without affecting the actual users or the live system
+
+        //===== 2. Staging Enirment =========
+        //The staging environment is used as a pre-production environment that closely resembles the production environment. It serves as a testing ground where the application is deployed before being released to production. 
+
+        //===== 3. Production Enirment =========
+        //The production environment is where the application is deployed and made available to end-users.
+        #endregion
+
+
+        //=========================================================================
+        //========================== Section = Configuration (Importent) ==========================
+        //=========================================================================
+        #region Configuration_in_ProgramFile
+        //Please go to program file
+        #endregion
     }
 }
